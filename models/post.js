3b4d27dae7230fc8aaae5aac3ff8e13e22dc8313
Dipesh Valia <dvalia@walmartlabs.com>
2015-05-11 23:13:56 -0700

    upload project

--- models/post.js
+++ models/post.js
@@ -5,6 +5,18 @@ let mongoose = require('mongoose')
 
 require ('songbird')
 
+let commentSchema = mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    }, 
+    createDate: {
+        type: Date,
+        default: Date.now
+    },
+    username: String
+})
+
 let postSchema = mongoose.Schema({
     title: {
     	type: String,
@@ -17,7 +29,29 @@ let postSchema = mongoose.Schema({
    	image: {
       data: Buffer,
       contentType: String
-    }
+    },
+
+    createDate: {
+      type: String,
+      default: Date.now
+    },
+    modifiedDate: {
+      type: String,
+      default: Date.now
+    },
+    count: {
+      type: String,
+      // required: true
+    },
+    userId: mongoose.Schema.ObjectId,
+    comments: [commentSchema],
+})
+
+
+
+postSchema.pre('save', function(next) {
+    this.updated = Date.now()
+    next()
 })
 
 module.exports = mongoose.model('Post', postSchema)
--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ let fs = require('fs')
 let multiparty = require('multiparty')
 let then = require('express-then')
 let isLoggedIn = require('./middleware/isLoggedIn')
-let passport = require('passport')
+let passport = require('./middleware/passport')
 let Post = require('./models/post')
 let DataUri = require('datauri')
 
@@ -40,14 +40,50 @@ module.exports = (app) => {
 	//      res.redirect('/')
 	// }
 
-	app.get('/profile', isLoggedIn, (req, res) => res.render('profile.ejs', {
+	// app.get('/profile', isLoggedIn, (req, res) => res.render('profile.ejs', {
 
-		// email: req.user.email,
-		// id: req.user.id,
-		// password: req.user.password
-		user: req.user,
-		message: req.flash('error')
-	}))
+	// 	// email: req.user.email,
+	// 	// id: req.user.id,
+	// 	// password: req.user.password
+	// 	user: req.user,
+
+	// 	message: req.flash('error')
+	// }))
+
+	app.get('/profile', isLoggedIn, then(async(req, res) => {
+        let posts = await Post.promise.find({
+            userId: req.user.id
+        })
+
+        let comments = []
+        for (let post of posts) {
+            if (post.comments && post.comments.length > 0) {
+
+            	console.log("***** Comment.username:" + comment.username)
+            	console.log("***** Comment.createDate:" + comment.createDate)
+            	console.log("***** Post.comments:" + post.comment)
+            	console.log("***** Post.createDate:" + post.createDate)
+            	console.log("***** Post.modifiedDate:" + post.modifiedDate)
+            	console.log("***** Post.title:" + post.title)
+                // take the last comment in the array as the latest comment
+                let comment = post.comments[post.comments.length - 1]
+
+                comments.push({
+                    content: comment.content.substr(0, 124),
+                    username: comment.username,
+                    created: comment.createDate,
+                    postLink: "/post/" + post.id
+                })
+            }
+        }
+        res.render('profile.ejs', {
+            user: req.user,
+            posts: posts,
+            comments: comments,
+            message: req.flash('error'),
+
+        })
+    }))
 
 	app.get('/logout', (req, res) =>{
 		// console.log("******1");
@@ -59,6 +95,7 @@ module.exports = (app) => {
 	app.get('/post/:postId?', then( async (req, res) =>{
 	
 		 let postId = req.params.postId
+		 let reqUserId = req.user ? req.user.id : null
 		 if(!postId) {
 
 		 		res.render('post.ejs', {
@@ -75,10 +112,13 @@ module.exports = (app) => {
 		let image = dataUri.format('.' + post.image.contentType.split('/').pop(), post.image.data)
 
 		//console.log(image)
-		res.render('post.ejs', {
+		res.render('blogger.ejs', {
 			post: post,
 			verb: 'Edit',
-			image: 'data:${post.image.contentType};base64,${image.base64}'
+			// image: 'data:${post.image.contentType};base64,${image.base64}'
+			image: `data:${post.image.contentType};base64,${image.base64}`,
+			requestUserId: reqUserId,
+			canEdit: reqUserId && (reqUserId == post.userId)
 		})
 
 	}))
@@ -100,12 +140,16 @@ module.exports = (app) => {
 
 	 	post.image.data = await fs.promise.readFile(file.path)
 	 	post.image.contentType = file.headers['content-type']
+
+	 	
 	 	await post.save()
 
 	 	//console.log("****** Blog Title " + req.user.blogTitle)
 	 	
 	 	// res.redirect('/blog/' + encodeURI(req.user.blogTitle))
-	 	res.redirect('/blog/' + "some data")
+	 	//res.redirect('/blog/' + "some data")
+	 	postId = post.id
+        res.redirect('/post/' + postId)
 
 	 	return
 
@@ -121,8 +165,96 @@ module.exports = (app) => {
 	 post.content = content
 	 await post.save()
 	// res.redirect('/blog/' + encodeURI(req.user.blogTitle))
-	 res.redirect('/blog/' + "some data")
+	 //res.redirect('/blog/' + "some data")
+	 postId = post.id
+     res.redirect('/post/' + postId)
 
 	 
 	}))
+
+	app.post('/comment', isLoggedIn, then(async(req, res) => {
+
+        let post = await Post.promise.findById(req.body.postId)
+        if (post) {
+            post.comments.push({
+                content: req.body.comment,
+                username: req.user.username
+            })
+            await post.save()
+        }
+
+        res.redirect('/post/' + req.body.postId)
+    }))
+
+    app.get('/post/edit/:postId?', then(async(req, res) => {
+        let postId = req.params.postId
+        let requestUserId = req.user ? req.user.id : null
+        if (!postId) {
+            res.render('edit.ejs', {
+                post: {
+                    comments: []
+                },
+                verb: 'Create'
+            })
+            return
+        }
+        let post = await Post.promise.findById(postId)
+        if (!post) res.status(404).send('Not found')
+
+        let dataUri = new DataUri()
+        let image
+        let imageData
+        if (post.image.data) {
+            image = dataUri.format('.' + post.image.contentType.split('/').pop(), post.image.data)
+            imageData = `data:${post.image.contentType};base64,${image.base64}`
+        }
+        return res.render('edit.ejs', {
+            post: post,
+            verb: 'Edit',
+            image: imageData,
+            requestUserId: requestUserId
+        })
+    }))
+
+    app.post('/logincomment', passport.authenticate('local-login'), then(async(req, res) => {
+
+        // get the post document
+        let post = await Post.promise.findById(req.body.postId)
+        if (post) {
+            post.comments.push({
+                content: req.body.comment,
+                username: req.user.username
+            })
+            await post.save()
+        }
+
+        res.redirect('/post/' + req.body.postId)
+    }))
+
+    app.delete('/post/:postId', isLoggedIn, (req, res) => {
+        async() => {
+            let postId = req.params.postId
+            let post = await Post.promise.findById(postId)
+            if (post) await post.promise.remove()
+            res.end()
+        }().catch(e => console.log('err', e))
+    })
+
+    //get all posts
+    app.get('/posts', then(async(req, res) => {
+        let posts = await Post.promise.find({})
+        let requestUserId = req.user ? req.user.id : null
+        let dataUri = new DataUri()
+        for (var post of posts) {
+            if (post.image.data) {
+                let image = dataUri.format('.' + post.image.contentType.split('/').pop(), post.image.data)
+                post.imageData = `data:${post.image.contentType};base64,${image.base64}`
+            }
+        }
+        res.render('allposts.ejs', {
+            posts: posts,
+            requestUserId: requestUserId,
+            blogUserId: null
+        })
+    }))
 }
